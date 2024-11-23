22ffd17dd9a86fa36b2e173f00e70f0915418d4d
Erika Nelson <erky@Erikas-MacBook-Air.local>
2024-11-23 15:46:13 -0500

    deleted gdscripts for deleted nodes

--- scenes/unhealth_tester.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-@onready var player: Player = $"../Player"
-
-
-func _on_body_entered(body: Node2D) -> void:
-	print ("ouuuucchhh!!")
-	player.subtract_health(10)
-	
--- scripts/area_2d.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-@onready var player: Player = $"../Player"
-
-func _on_body_entered(body):
-	print("add_health")
-	player.add_health(10)
